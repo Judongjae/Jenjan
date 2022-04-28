b07b97a77ec3966da2a18c38add6f9678a11b599
Judongjae <wnehdwo0982@naver.com>
2022-04-28 20:44:16 +0900

    aws RDS연결

--- app/app.js
+++ app/app.js
@@ -1,18 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const { response } = require("express");
 // const morgan = require("morgan");
 // const logger = require("");
 const app = express();
 const router = express.Router();
 dotenv.config();
 
-const port = 3000;
-
-app.listen(port, () => {
-  console.log("서버가 실행됨");
-});
-
 app.get("/", (req, res) => {
-  response.send("yy");
+  res.send("yy");
 });
+
+module.exports = app;
