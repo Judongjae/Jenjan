417a518691c7eac34547562753f9d95ad7f46df4
Judongjae <wnehdwo0982@naver.com>
2022-05-02 20:59:34 +0900

    라우터에서 컨트롤러 분리

--- app/app.js
+++ app/app.js
@@ -5,9 +5,7 @@ const dotenv = require("dotenv");
 const app = express();
 dotenv.config();
 
-const home = require("./src/routes/home"); //home은 저 위치에 있다
-
 app.set("views", "./src/views"); //view 엔진경로
 app.set("view engine", "ejs");
-app.use("/", home); // '/'여기로 오면 home이 실행된다.
+
 module.exports = app;
