058200838c378ba6be89768e1820c8675cce163f
Judongjae <wnehdwo0982@naver.com>
2022-05-06 20:42:39 +0900

    왜 안될까

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,3 +1,4 @@
+const { resolveInclude } = require("ejs");
 const db = require("../../config/db");
 const sql = "INSERT INTO practice(yy) VALUES('yoyo')";
 
@@ -6,17 +7,33 @@ const output = {
     res.send("home");
   },
   login: (req, res) => {
-    res.send("login");
+    return new Promise((res, rej) => {
+      const query = "SELECT * FROM practice;";
+      db.query(query, (err, data) => {
+        console.log(data);
+      });
+    });
   },
 };
 
 const process = {
   login: (req, res) => {
-    const { yy } = req.body;
-    console.log(`${yy}`);
-    res.send(`${yy}`);
+    db.query("SELECT * FROM practice;", (err, data) => {
+      console.log(data);
+      res.send("post login");
+    });
   },
 };
+
+// const process = {
+//   login: (req, res) => {
+//     const { yy } = req.body;
+//     console.log(`${yy}`);
+//     res.send(`${yy}`);
+//   },
+// };
+
+const query = "INSERT INTO practice (yy) VALUES (?);";
 
 module.exports = {
   output,
