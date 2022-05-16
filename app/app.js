27c2a78b86ab7915d1ffbc0336b61d58f4e0f41d
Judongjae <wnehdwo0982@naver.com>
2022-05-16 18:49:44 +0900

    쿠키적용

--- app/app.js
+++ app/app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
 const app = express();
+
 dotenv.config();
 
 const home = require("./src/routes/home");
 
+app.use(cookieParser());
 app.use(express.json());
 app.use(
   express.urlencoded({
